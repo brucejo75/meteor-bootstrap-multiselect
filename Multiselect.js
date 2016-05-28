8d12f3866d93b81f2a2de65ec8c04e2b9f0d6bb5
brucejo <brucejo.wk@outlook.com>
2016-05-27 17:01:11 -0700

    Issue #1, make sure multiselect is initialized after DOM is fully complete

--- Multiselect.js
+++ Multiselect.js
@@ -1,12 +1,22 @@
-
-Template.Multiselect.onRendered(function() {
+Template.Multiselect.onRendered(function multiselectOnRendered() {
   let template = this;
   let config = {};
   if(template.data.configOptions) {
     config = template.data.configOptions;
   }
 
-  this.$('select').multiselect(config);
+  // autorun waits until after the dependent data has been updated
+  template.autorun(function multiselectAutorun() {
+    Template.currentData();
+    // afterFlush waits until the rest of the DOM elements have been built
+    Tracker.afterFlush(function multiselectAfterFlush() {
+      if(template.data.menuItems.length > 0) {
+        // Finally ready to initialize the multiselect
+        // e.g. after #each has completed creating all elements
+        template.$('select').multiselect(config);
+      }
+    });
+  });
 });
 
 Template.Multiselect.helpers({
@@ -21,4 +31,3 @@ Template.Multiselect.helpers({
     return _.extend({}, this, {'selectedAttr': selected ? 'selected' : ''});
   }
 });
-
