dedb30cf4bd25e3aec88469c69a2b169c9928d3d
brucejo <brucejo.wk@outlook.com>
2016-05-28 04:34:43 -0700

    Publish v0.0.3

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   'name': 'brucejo:bootstrap-multiselect',
-  'version': '0.0.2',
+  'version': '0.0.3',
   'summary': 'Bootstrap Multiselect wrapper for Meteor',
   'git': 'https://github.com/brucejo75/meteor-bootstrap-multiselect.git',
   'documentation': 'README.md'
